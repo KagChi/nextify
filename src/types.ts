1b5afbaca7127c4af42b0f70b25cc8fc974906fc
KagChi <yonuxbot@gmail.com>
2024-07-23 23:51:39 +0700

    chore: update typings

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
-import React from "react";
+import type React from "react";
 
-export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
-export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
 export interface BaseConditionOptions {
     when: boolean;
